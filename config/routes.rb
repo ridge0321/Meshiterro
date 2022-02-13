2c448e0709441981db26ba2542cd718178797275
EC2 Default User <ec2-user@ip-172-31-6-219.ec2.internal>
2022-02-13 02:49:57 +0000

    chapter16

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get 'homes/about'=>'homes#about' ,as:'about'
+
+    # 以下の行のonly: []内にupdateを追加
+  resources :users, only: [:show, :edit, :update]
+
 end
