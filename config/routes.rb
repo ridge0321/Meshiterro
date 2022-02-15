87183bb50fa909f085096e0dd5ca04ce48b3430b
EC2 Default User <ec2-user@ip-172-31-6-219.ec2.internal>
2022-02-15 01:52:41 +0000

    chapter19

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'homes#top'
   devise_for :users
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  get 'homes/about'=>'homes#about' ,as:'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
 
-    # 以下の行のonly: []内にupdateを追加
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 
-end
+  end
